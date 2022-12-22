d99cbb6b0216babbec1aa743b32cc87afed023bf
Abhishek Mishra <abhiusedu@gmail.com>
2022-12-22 01:11:47 -0800

    Update ASDF install script

--- ubuntu-install-asdf.sh
+++ ubuntu-install-asdf.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
 
+echo "Install apt modules"
+sudo apt install -y curl git
+
+echo "Install asdf"
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+
+echo "Update oh-my-zsh plugins"
+sed -i -e 's/^\(plugins=(.*\))$/\1 asdf)/' ~/.zshrc
+
+zsh <<UPDATE
+    asdf update
+UPDATE
